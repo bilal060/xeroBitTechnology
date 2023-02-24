745ded96fac908424b7d79361a1b9d1bb6c4e3c5
Muhammad Numan <numanrasheed315@gmail.com>
2023-02-24 14:38:32 +0500

    add html code

---
+++ pages/home/index.tsx
@@ -0,0 +1,14 @@
+import { Inter } from 'next/font/google'
+import Card from "component/component/card";
+
+
+export default function Home() {
+    return (
+        <>
+          <div>
+              Numan
+              <Card/>
+          </div>
+        </>
+    )
+}
