ac6b718ed5ecbfa929c39bf3778192074ea58dd7
Muhammad Numan <numanrasheed315@gmail.com>
2023-02-24 14:34:01 +0500

    add bootstrap

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,4 +1,6 @@
 import 'component/styles/globals.css'
+import "bootstrap/dist/css/bootstrap.min.css"; // Import bootstrap CSS
+
 import type { AppProps } from 'next/app'
 
 export default function App({ Component, pageProps }: AppProps) {
