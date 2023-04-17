2628a0aeb62d6d9ead7ab89840f7c5410241de26
Muhammad Bilal <bilal@dinxsolutions.com>
2023-04-17 12:07:43 +0500

    Blog API Integration

---
+++ component/APICall.tsx
@@ -0,0 +1,21 @@
+import axios from "axios";
+
+const url = process.env.API_URL || "http://localhost:3500";
+const CallAPI = async (data) => {
+  try {        
+    return await axios
+      .get(`${url}/${data}`)
+      .then(function (response) {
+        return response;
+      })
+      .catch(function (error) {
+        console.log(error);
+        return error;
+      })
+      .finally(function () {});
+  } catch (error) {
+    console.error(error);
+  }
+};
+
+export default CallAPI;
